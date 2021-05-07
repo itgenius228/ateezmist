d5baac37268b1dc73a215ea879a10f12b85fedc6
itgenius228 <83812360+itgenius228@users.noreply.github.com>
2021-05-07 13:22:19 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 
-var canvas = document.getElementById("game")
-var context = canvas.getContext("2d")
+var canvas = document.getElementById('game')
+var context = canvas.getContext('2d')
 
 var x = 0;
 var y = 0;
+var direction = 'down'
 
 
 function play() {
     context.clearRect(0, 0, 600, 500);
     context.fillRect(x, y, 20, 20);
+
 
    var button = keyboard.button();
    console.log(button)
